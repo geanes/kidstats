4befdd1b735653e27fb48c3712ae8600736f4a0f
geanes <glenn.eanes@gmail.com>
2015-03-06 02:33:44 -0600

    Refactor ksapp

--- inst/ksapp/server.R
+++ inst/ksapp/server.R
@@ -2,92 +2,88 @@
 shinyServer(function(input, output) {
 
  refdata <- reactive({
-   infile <- input$refsamp
+   infile <- paste0("~/stats/kidstats/data/", input$refsamp)
 
    if (is.null(infile))
    return(NULL)
 
-   ref <- get(load(infile$datapath))
+   ref <- get(load(infile))
    return(ref)
  })
 
-
+# elements
  elements <- reactive({
    elements <-data.frame()
 
-    if((input$all | input$femur) & !is.na(input$fmxl)){
+    if(!is.na(input$fmxl)){
       elements[nrow(elements) + 1, 1] <- "f_mxl"
       elements[nrow(elements), 2] <- input$fmxl
     }
-   #  if((input$all | input$femur) & is.na(input$fpb)){
-   #    elements[nrow(elements) + 1, 1] <- "f_pb"
-   #    elements[nrow(elements), 2] <- input$fpb
-   #  }
-    if((input$all | input$femur) & !is.na(input$fmsb)){
+    if(!is.na(input$fmsb)){
       elements[nrow(elements) + 1, 1] <- "f_msb"
       elements[nrow(elements), 2] <- input$fmsb
     }
-    if((input$all | input$femur) & !is.na(input$fdb)){
+    if(!is.na(input$fdb)){
       elements[nrow(elements) + 1, 1] <- "f_db"
       elements[nrow(elements), 2] <- input$fdb
     }
 
-    if((input$all | input$tibia) & !is.na(input$tmxl)){
+    if(!is.na(input$tmxl)){
       elements[nrow(elements) + 1, 1] <- "t_mxl"
       elements[nrow(elements), 2] <- input$tmxl
     }
-    if((input$all | input$tibia) & !is.na(input$tpb)){
+    if(!is.na(input$tpb)){
       elements[nrow(elements) + 1, 1] <- "t_pb"
       elements[nrow(elements), 2] <- input$tpb
     }
-    if((input$all | input$tibia) & !is.na(input$tmsb)){
+    if(!is.na(input$tmsb)){
       elements[nrow(elements) + 1, 1] <- "t_msb"
       elements[nrow(elements), 2] <- input$tmsb
     }
-    if((input$all | input$tibia) & !is.na(input$tdb)){
+    if(!is.na(input$tdb)){
       elements[nrow(elements) + 1, 1] <- "t_db"
       elements[nrow(elements), 2] <- input$tdb
     }
 
-    if((input$all | input$humerus) & !is.na(input$hmxl)){
+    if(!is.na(input$hmxl)){
       elements[nrow(elements) + 1, 1] <- "h_mxl"
       elements[nrow(elements), 2] <- input$hmxl
     }
-    if((input$all | input$humerus) & !is.na(input$hpb)){
+    if(!is.na(input$hpb)){
       elements[nrow(elements) + 1, 1] <- "h_pb"
       elements[nrow(elements), 2] <- input$hpb
     }
-    if((input$all | input$humerus) & !is.na(input$hmsb)){
+    if(!is.na(input$hmsb)){
       elements[nrow(elements) + 1, 1] <- "h_msb"
       elements[nrow(elements), 2] <- input$hmsb
     }
-    if((input$all | input$humerus) & !is.na(input$hdb)){
+    if(!is.na(input$hdb)){
       elements[nrow(elements) + 1, 1] <- "h_db"
       elements[nrow(elements), 2] <- input$hdb
     }
 
-    if((input$all | input$radius) & !is.na(input$rmxl)){
+    if(!is.na(input$rmxl)){
       elements[nrow(elements) + 1, 1] <- "r_mxl"
       elements[nrow(elements), 2] <- input$rmxl
     }
-    if((input$all | input$radius) & !is.na(input$rpb)){
+    if(!is.na(input$rpb)){
       elements[nrow(elements) + 1, 1] <- "r_pb"
       elements[nrow(elements), 2] <- input$rpb
     }
-    if((input$all | input$radius) & !is.na(input$rmsb)){
+    if(!is.na(input$rmsb)){
       elements[nrow(elements) + 1, 1] <- "r_msb"
       elements[nrow(elements), 2] <- input$rmsb
     }
-    if((input$all | input$radius) & !is.na(input$rdb)){
+    if(!is.na(input$rdb)){
       elements[nrow(elements) + 1, 1] <- "r_db"
       elements[nrow(elements), 2] <- input$rdb
     }
 
-    if((input$all | input$ulna) & !is.na(input$umxl)){
+    if(!is.na(input$umxl)){
       elements[nrow(elements) + 1, 1] <- "u_mxl"
       elements[nrow(elements), 2] <- input$umxl
     }
-    if((input$all | input$ulna) & !is.na(input$umsb)){
+    if(!is.na(input$umsb)){
       elements[nrow(elements) + 1, 1] <- "u_msb"
       elements[nrow(elements), 2] <- input$umsb
     }
@@ -96,11 +92,44 @@ shinyServer(function(input, output) {
 
     return(elements)
  })
+# construct elements table
+    el_names <- c("<h4>Elements</h4>", "<h5>Femur</h5>", "<h5>Tibia</h5>", "<h5>Humerus</h5>", "<h5>Radius</h5>", "<h5>Ulna</h5>")
+    el_meas <- c("Max. Length", "Prox. Breadth", "MS. Breadth", "Dist. Breadth")
+      mxl <- c("Max. Length",
+        "<input id='fmxl' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='tmxl' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='hmxl' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='rmxl' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='umxl' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>"
+      )
+      pb <- c("Prox. Breadth",
+        NA,
+        "<input id='tpb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='hpb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='rpb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        NA
+      )
+      msb <- c("MS. Breadth",
+        "<input id='fmsb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='tmsb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='hmsb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='rmsb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='umsb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>"
+      )
+      db <- c("Dist. Breadth",
+        "<input id='fdb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='tdb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='hdb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        "<input id='rdb' class='shiny-bound-input' type='number' value='NA' min='0' max='1000'>",
+        NA
+      )
+        output$el_table <-renderTable({
+          data.frame(el_names, mxl, pb, msb, db)
+        }, sanitize.text.function = function(x) x, sanitize.rownames.function = function(x) x, sanitize.colnames.function = function(x) x, include.rownames = FALSE, include.colnames = FALSE)
 
 
   refsamp <- reactive({
     if(is.null(refdata()) | is.null(elements())) return(NULL)
-
     data <- refdata()
     dat <- elements()
     dat_clean <- data[c("age_y", dat[[1]])]
@@ -111,17 +140,14 @@ shinyServer(function(input, output) {
 
   output$table <- renderDataTable({
     if(is.null(refsamp())) return(NULL)
-
     refsamp()
   })
 
 
   earth_mod <- reactive({
-
     dat <- elements()
     ref <- refsamp()
     x <- ref[, -1]
-
     newdata <- as.data.frame(t(dat[, -1]))
     names(newdata) <- dat[[1]]
 
--- inst/ksapp/ui.R
+++ inst/ksapp/ui.R
@@ -1,152 +1,36 @@
 #' @import shiny
-shinyUI(
-  navbarPage("kidstats", id = "nav", windowTitle = "kidstats", collapsable = FALSE,
-   inverse = TRUE, theme = "kidstats_bootstrap.css", fluid = FALSE,
-
-  footer=HTML("
-    <div class = 'navbar navbar-inverse navbar-fixed-bottom' style='line-height:30px; height:30px;'>
-      <div class = 'navbar-inner'>
-        <div class = 'container footer-margin-top'>
-          <p class = 'pull-right' style='color: white;'>&copy; Kyra Stull  2014</p>
-        </div>
-      </div>
-    </div>"),
-
-
+library(shiny)
+shinyUI(navbarPage(title = "kidstats", windowTitle = "kidstats",
+                   position = "fixed-top", inverse = TRUE, collapsible = TRUE,
+                   header = tags$style(type="text/css", "body {padding-top: 70px;}"),
   tabPanel("Input", value = "input",
-
-
-  fluidPage(
-
-  wellPanel(
-  fluidRow(
-    column(4, fileInput("refsamp", label = h4("Reference data"))),
-    column(1, offset = 1, br(),
-      checkboxInput("all", label = strong("All"), value = FALSE),
-      checkboxInput("femur", label = strong("Femur"), value = FALSE),
-      checkboxInput("tibia", label = strong("Tibia"), value = FALSE)),
-    column(1, br(),
-      checkboxInput("humerus", label = strong("Humerus"), value = FALSE),
-      checkboxInput("radius", label = strong("Radius"), value = FALSE),
-      checkboxInput("ulna", label = strong("Ulna"), value = FALSE)),
-    column(3, offset = 2,
-      radioButtons("transform", label = h4("Transformation"),
+# Sidebar
+    sidebarPanel(
+      selectizeInput("refsamp", "Reference Sample", choices = c("South Africa" = "salb_za.rda"), selected = "salb_za.rda", multiple = TRUE, width = "200px"),
+# fileInput("refsamp", label = h4("Reference data")),
+      radioButtons("transform", label = "Transformation",
         choices = list("None" = "none", "Square root" = "sqrt", "Cube root" = "cbrt"),
         selected = "none")
-    )
-  )),
-
-  wellPanel(
-
-  conditionalPanel("input.all == false & input.femur == false & input.tibia == false & input.humerus == false & input.radius == false & input.ulna == false",
-    fluidRow(column(6, offset = 4, "Choose element(s) above."))
-  ),
-
-  # fluidRow(column(4, offset = 4, "(Measurements in mm)")),
-
-  conditionalPanel("input.all == true | input.femur == true | input.tibia == true | input.humerus == true | input.radius == true | input.ulna == true",
-  fluidRow(
-  column(2, offset = 1, h5(style = "line-height = 10", "Maximum length")),
-  column(2, offset = 1, h5(style = "line-height = 10", "Proximal breadth")),
-  column(2, offset = 1, h5(style = "line-height = 10", "Mid-shaft breadth")),
-  column(2, offset = 1, h5(style = "line-height = 10", "Distal breadth"))
-  )),
-
-  conditionalPanel("input.femur == true | input.all == true",
-  fluidRow(
-    column(1, h5("Femur")),
-    column(2,
-           numericInput("fmxl", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    # column(2, offset = 1,
-    #       numericInput("fpb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 4,
-           numericInput("fmsb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("fdb", label = "", value = NA, min = 0, max = 1000, step = 1))
-  )),
-
-
-  conditionalPanel("input.tibia == true | input.all == true",
-  fluidRow(
-    column(1, h5("Tibia")),
-    column(2,
-           numericInput("tmxl", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("tpb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("tmsb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("tdb", label = "", value = NA, min = 0, max = 1000, step = 1))
-  )),
-
-
-  conditionalPanel("input.humerus == true | input.all == true",
-  fluidRow(
-    column(1, h5("Humerus")),
-    column(2,
-           numericInput("hmxl", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("hpb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("hmsb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("hdb", label = "", value = NA, min = 0, max = 1000, step = 1))
-  )),
-
-
-  conditionalPanel("input.radius == true | input.all == true",
-  fluidRow(
-    column(1, h5("Radius")),
-    column(2,
-           numericInput("rmxl", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("rpb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("rmsb", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 1,
-           numericInput("rdb", label = "", value = NA, min = 0, max = 1000, step = 1))
-  )),
-
-
-  conditionalPanel("input.ulna == true | input.all == true",
-  fluidRow(
-    column(1, h5("Ulna")),
-    column(2,
-           numericInput("umxl", label = "", value = NA, min = 0, max = 1000, step = 1)),
-    column(2, offset = 4,
-           numericInput("umsb", label = "", value = NA, min = 0, max = 1000, step = 1))
-  ))),
-
-  br(),br(), br(), br(),
-
-  absolutePanel(bottom = 30, left = 0, right = 0, fixed = TRUE,
-  div(style="padding: 4px; border-top: 1px solid #CCC; background: #FFFFEE;",
-  fluidRow(
-    # column(2,  actionButton("estimate", label = "Estimate age")),
-    column(3, offset = 1, htmlOutput("age")),
-    column(3, offset = 1, htmlOutput("rsq")),
-    column(3, offset = 1, htmlOutput("sampsize"))
+    ),
+# Main panel
+   mainPanel(fluidRow(column(10, offset = 1, tableOutput("el_table"))),
+# quick results panel
+    wellPanel(fluidRow(
+      column(4, htmlOutput("age")),
+      column(4, htmlOutput("rsq")),
+      column(4, htmlOutput("sampsize"))
     )))
-
-)),
-
-
+  ),
 # tabPanel("Output", fluidRow(
 #
 # )),
 
-tabPanel("Reference Sample", fluidRow(
-  dataTableOutput("table")
-)),
-
+# Reference Sample table
+  tabPanel("Reference Sample", fluidRow(
+    column(10, offset = 1, dataTableOutput("table"))
+  ))
 
 # tabPanel("About", fluidRow(
 #   column(12, includeMarkdown("about.md"))
-# )),
-
-br(), br()
-
-
+# ))
 ))
-
-
