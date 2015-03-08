c4cf55b5d153a3ca5f3cd0c23bae6d08302075de
geanes <glenn.eanes@gmail.com>
2015-03-08 04:40:50 -0500

    Add documentation

--- R/kidstats.R
+++ R/kidstats.R
@@ -1,4 +1,29 @@
+#' kidstats: Shiny application to make sub-adult age and sex estimations.
+#'
+#' \code{kidstats} Shiny application to make sub-adult age and sex estimations
+#' based on long-bone measurements using MARS models.
+#'
+#' @details
+#' This package contains the following datasets:
+#' \itemize{
+#'   \item \code{salb_za}: Sub-adult long bone length measurements
+#'    (South Africa)
+#' }
+#'
+#' and the following functions:
+#' \itemize{
+#'   \item \code{kidstats}: Runs the kidstats shiny app.
+#' }
+#'
+#' @docType package
+#' @name kidstats
+NULL
+
 #' @import shiny
+#' Kidstats shiny app call
+#'
+#' @examples
+#' kidstats()
 #' @export
 kidstats <- function(){
   shiny::runApp(system.file('ksapp', package = 'kidstats'))
